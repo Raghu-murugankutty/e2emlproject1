162d85b0a76e1192bcdf145de7cb40b60e5d2c19
Raghu-murugankutty <Raghu.murugankutty5@gmail.com>
2023-10-21 18:10:32 +0100

    'notebookandEDAmodeltraining'

--- src/exception.py
+++ src/exception.py
@@ -17,9 +17,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 10/0
-    except Exception as e:
-        logging.info("Devided by zero")
-        raise CustomException(e, sys)
+# if __name__ == "__main__":
+#     try:
+#         a = 10/0
+#     except Exception as e:
+#         logging.info("Devided by zero")
+#         raise CustomException(e, sys)
